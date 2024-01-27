27bc881ba5d1f6e336dd60113afdc8a93eb8b969
blitzes27 <153459266+blitzes27@users.noreply.github.com>
2024-01-27 09:08:16 +0100

    Update gitclone.sh

--- gitclone.sh
+++ gitclone.sh
@@ -12,5 +12,17 @@ sleep 20
 
 cd "wordpress"
 
-#kör installationsskript i wordpress mapp
-./install.sh
+#skapar en dum container joinad nätverket uppgift för att kunna
+#ansluta med vår wordpress
+docker run --name some-postgres –network uppgift -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+
+sleep 8
+
+
+    # Packa upp zip-filen
+    unzip "test3"
+
+        cd "test3"
+
+        # Kör docker-compose up -d
+        docker compose up -d
